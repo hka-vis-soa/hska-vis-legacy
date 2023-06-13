ab15b48f543c25abc64ed10f9b57bfea1155555f
dns <dennis.zhukki@gmail.com>
2023-06-13 17:15:36 +0200

    category methods

--- src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/CategoryManagerImpl.java
+++ src/main/java/hska/iwi/eShopMaster/model/businessLogic/manager/impl/CategoryManagerImpl.java
@@ -3,7 +3,9 @@ package hska.iwi.eShopMaster.model.businessLogic.manager.impl;
 
 import hska.iwi.eShopMaster.model.businessLogic.manager.CategoryManager;
 import hska.iwi.eShopMaster.model.database.dataobjects.Category;
+import hska.iwi.eShopMaster.model.webclient.HttpMethod;
 import hska.iwi.eShopMaster.model.webclient.RequestClient;
+import hska.iwi.eShopMaster.model.webclient.URL;
 import org.apache.commons.lang3.NotImplementedException;
 
 import java.util.List;
@@ -17,10 +19,14 @@ public class CategoryManagerImpl implements CategoryManager {
     }
 
     public List<Category> getCategories() {
+        String targetURL = URL.HOST + URL.CATEGORIES;
+        String response = requestClient.execute(HttpMethod.GET, targetURL, null, null);
         throw new NotImplementedException("getCategories#notImplemented");
     }
 
     public Category getCategory(int id) {
+        String targetURL = URL.HOST + URL.CATEGORIES + URL.SLASH + id;
+        String response = requestClient.execute(HttpMethod.GET, targetURL, null, null);
         throw new NotImplementedException("getCategory#notImplemented");
     }
 
@@ -29,6 +35,9 @@ public class CategoryManagerImpl implements CategoryManager {
     }
 
     public void addCategory(String name) {
+        String body = "{\"name\": '" + name + "\"}";
+        String targetURL = URL.HOST + URL.CATEGORIES;
+        String response = requestClient.execute(HttpMethod.POST, targetURL, null, body);
         throw new NotImplementedException("addCategory#notImplemented");
 
     }
---
+++ src/main/java/hska/iwi/eShopMaster/model/webclient/URL.java
@@ -0,0 +1,21 @@
+package hska.iwi.eShopMaster.model.webclient;
+
+public class URL {
+
+    public static final String HOST = "http://localhost:8080";
+
+    // category-service URLs
+    public static final String CATEGORIES = "/categories";
+    // product-service URLs
+    public static final String PRODUCTS = "/products";
+    public static final String MINPRICE = "minPrice=";
+    public static final String MAXPRICE = "maxPrice=";
+    public static final String DETAILS = "details=";
+    public static final String CATEGORY = "/category";
+    // Helper
+    public static final String EQUALS = "=";
+    public static final String QUESTIONMARK = "?";
+
+    public static final String AND = "&";
+    public static final String SLASH = "/";
+}
